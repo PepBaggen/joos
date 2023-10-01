93254a4e62e1328d53bf0decdcccabb28311ae49
PepBaggen <pepijnbaggen@gmail.com>
2023-10-01 22:08:30 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,130 @@
+
+<head>
+</head>
+<meta charset="UTF-8">
+<title>Josephine Baggen</title>
+<style>
+        @import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+       
+        .navbar {
+            display: flex;
+            font-family: 'Montserrat', sans-serif;
+            justify-content: center;
+            padding: 1vh;
+            font-size: 1.5vw;   
+                
+
+        }
+
+        .navbar a {
+            color: white;
+            text-decoration: none;
+            margin: 0 10px;
+            font-weight: bold;
+        }
+
+        .navbar a:hover {
+            color: #66D9EF;
+        }
+        
+        
+        
+        
+        html, body {
+            height: 100%;
+            margin: 0;
+            padding: 0;
+
+        }
+
+        body {
+            background-image: url('https://images.hdqwalls.com/download/mars-planet-5k-eu-1920x1080.jpg');
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-position: center center;
+            min-height: 100vh;
+            object-fit: cover; 
+        }
+        h1 {
+            text-align: center;
+            padding-top: 35vh;
+            font-size: 4vw;            
+            font-family: 'Montserrat', sans-serif;
+            color: white; 
+
+        }
+        h2{
+            text-align: center;
+            padding-left: 12%;
+            padding-right: 12%;
+            font-size:1vw;
+            font-family: 'Montserrat', sans-serif;
+            color: white; 
+        }
+
+
+
+
+
+        @media screen and (min-width: 600px) {
+            h1 {
+                font-size: 3.5vw; 
+            }
+            h2{
+                font: 5vw;
+            }
+        }
+
+        @media screen and (min-width: 900px) {
+            h1 {
+                font-size: 3vw; 
+            }
+        }
+
+        @media screen and (min-width: 1200px) {
+        h1 {
+                font-size: 2.5vw; 
+        }
+        .navbar{
+                font-size: 10px;
+                margin: 0 auto;
+            }
+        }
+
+        @media screen and (min-width: 1600px) {
+        h1 {
+            font-size: 1vw; 
+            }
+        .navbar {
+            font-size: 1.25vw; 
+            max-width: 1100px; 
+            margin: 0 auto; 
+        }
+
+        }
+
+
+    </style>
+
+<body>
+
+   
+    <div class="navbar">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Resume</a>
+        <a href="#">Works</a>
+        <a href="#">Contacts</a>
+    </div>
+    
+
+
+    <h1>
+        Josephine Baggen
+    </h1>
+    <h2>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    </h2>
+
+</body>
+
